67b438ff6b99368719a1c8cffc87ecd6b9be4492
CD Santiago <slashlos@gmail.com>
2017-06-03 08:50:10 -0400

    Initial Commit

---
+++ ScaryBugs/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ScaryBugs
+//
+//  Created by Carlos D. Santiago on 6/3/17.
+//  Copyright © 2017 Carlos D. Santiago. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
